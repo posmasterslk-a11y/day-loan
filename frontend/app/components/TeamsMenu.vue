2d53f1c369ce27568ffeceec9a36d487d04990c9
Diluka Chamara <codehub.software@gmail.com>
2026-06-04 17:12:33 +0530

    Remove system name text next to logo in menu

--- frontend/app/components/TeamsMenu.vue
+++ frontend/app/components/TeamsMenu.vue
@@ -7,6 +7,5 @@ defineProps<{
 <template>
   <div class="flex items-center gap-2 px-2" :class="[!collapsed && 'py-2']">
     <img src="/logo.png" alt="LoanMaster" class="h-8 object-contain" />
-    <span v-if="!collapsed" class="font-bold text-lg text-primary truncate">LoanMaster</span>
   </div>
 </template>
