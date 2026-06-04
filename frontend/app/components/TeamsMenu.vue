ca893af33c1b3b7895667393d7ea1aa005a778fa
Diluka Chamara <codehub.software@gmail.com>
2026-06-04 17:13:00 +0530

    Increase logo size in menu

--- frontend/app/components/TeamsMenu.vue
+++ frontend/app/components/TeamsMenu.vue
@@ -6,6 +6,6 @@ defineProps<{
 
 <template>
   <div class="flex items-center gap-2 px-2" :class="[!collapsed && 'py-2']">
-    <img src="/logo.png" alt="LoanMaster" class="h-8 object-contain" />
+    <img src="/logo.png" alt="LoanMaster" class="h-12 object-contain" />
   </div>
 </template>
